fbcaf46e5c4cf573a0b365c3bb143831e258813c
Ali-Master <ali_4286@live.com>
2016-11-16 18:01:53 +0330

    add unit test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+QUnit.test("billify", function(assert) {
+	function billTest(billId, billPayment, expected) {
+		var bill = new billify();
+		bill.setId(billId)
+		bill.setPeymentId(billPayment)
+		assert.equal(bill.getData().amount, expected);
+	}
+	billTest(2813823309056, 25143031, 25100);
+	billTest(2813823309056, 18143115, 18100);
+	billTest(2813823309056, 38743208, 38700);
+	billTest(2813823309056, 74952182, 74900);
+	billTest(2813823309056, 11652243, undefined);
+});
